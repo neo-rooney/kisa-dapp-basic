0f4283f9cdaa0fa91d532f17d74f2a1c16a017a3
rooney <bch3454@gmail.com>
2024-08-08 23:31:13 +0900

    feat: 카드 지갑 정보 분리

--- 1_start/front/src/components/VendingMachine.tsx
+++ 1_start/front/src/components/VendingMachine.tsx
@@ -12,29 +12,40 @@ import { Label } from "@radix-ui/react-label";
 
 const VendingMachine = () => {
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">🎂 컵 케이크 머신</CardTitle>
-        <CardDescription>지갑을 연결해주세요.</CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        {/* 이 곳에 지갑 UI를 넣어주세요. */}
-        <div className="grid gap-2">
-          <Label htmlFor="email">남은 컵 케이크 개수</Label>
-          <Input id="amount" />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="email">구매할 컵 케이크 수량을 입력해주세요.</Label>
-          <Input id="amount" required />
-        </div>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button className="w-full">구매하기</Button>
-        <Button className="w-full" variant="outline">
-          100개 리필하기
-        </Button>
-      </CardFooter>
-    </Card>
+    <div className="w-full max-w-sm grid gap-2">
+      <Card>
+        <CardHeader>
+          <div className="flex justify-between">
+            <CardTitle className="text-2xl">내 정보</CardTitle>
+          </div>
+          <CardDescription>지갑을 연결해주세요.</CardDescription>
+        </CardHeader>
+        <CardContent className="grid items-center gap-4">
+          {/* 이 곳에 지갑 UI를 넣어주세요. */}
+        </CardContent>
+      </Card>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">🎂 컵 케이크 머신</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">남은 컵 케이크 개수</Label>
+            <Input id="amount" />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">구매할 컵 케이크 수량을 입력해주세요.</Label>
+            <Input id="amount" required />
+          </div>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button className="w-full">구매하기</Button>
+          <Button className="w-full" variant="outline">
+            100개 리필하기
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
 
