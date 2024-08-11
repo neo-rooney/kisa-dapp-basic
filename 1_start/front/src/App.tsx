a4a076982cea93479346d3f8a676d14476d44441
rooney <bch3454@gmail.com>
2024-08-11 17:37:34 +0900

    feat: 프론트화면 퍼블리싱만 된 단계로 롤백

--- 1_start/front/src/App.tsx
+++ 1_start/front/src/App.tsx
@@ -1,12 +1,9 @@
 import VendingMachine from "@/components/vendingMachine";
-import { WalletContextProvider } from "@/contexts/WalletContext/WalletContext";
 
 function App() {
   return (
     <main className="w-full h-dvh flex items-center justify-center">
-      <WalletContextProvider>
-        <VendingMachine />
-      </WalletContextProvider>
+      <VendingMachine />
     </main>
   );
 }
